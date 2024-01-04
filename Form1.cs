81bbe56109f52fbd899897914f7482613b8858f8
gmdev86 <gmdev86@gmail.com>
2024-01-04 11:31:50 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ValidationBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var personValidator = new Validator<Person>()
+                .NotNull("Person must not be null.")
+                .MustBeTrue(person => person.Age >= 18, "Age must be 18 or older.")
+                .MustBeTrue(person => !string.IsNullOrEmpty(person.Name), "Name must not be empty.")
+                .LessThanOrEqual(person => person.Age, 100, "Age must be 100 or younger.");
+
+            var personToValidate = new Person { Name = "", Age = 15 };
+
+            List<ValidationError> validationErrors = personValidator.Validate(personToValidate);
+
+            if (validationErrors.Count == 0)
+            {
+                MessageBox.Show("Validation successful!");
+            }
+            else
+            {
+                foreach (var error in validationErrors)
+                {
+                    MessageBox.Show($"Property: {error.PropertyName}, Error: {error.ErrorMessage}");
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidationBuilder
+{
+    public class ValidationError
+    {
+        public string PropertyName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class Validator<T>
+    {
+        private readonly List<Func<T, bool>> _predicates = new List<Func<T, bool>>();
+        private readonly List<Func<T, bool>> _customValidationMethods = new List<Func<T, bool>>();
+        private readonly List<ValidationError> _validationErrors = new List<ValidationError>();
+
+        public Validator<T> AddRule(Func<T, bool> predicate, string errorMessage)
+        {
+            _predicates.Add(predicate);
+            return this;
+        }
+
+        public Validator<T> AddCustomValidation(Func<T, bool> customValidationMethod, string errorMessage)
+        {
+            _customValidationMethods.Add(customValidationMethod);
+            return this;
+        }
+
+        public Validator<T> MustBeTrue(Func<T, bool> condition, string errorMessage)
+        {
+            return AddRule(condition, errorMessage);
+        }
+
+        public Validator<T> NotNull(string errorMessage)
+        {
+            return AddRule(value => value != null, errorMessage);
+        }
+
+        public Validator<T> LessThanOrEqual(Func<T, IComparable> propertySelector, IComparable value, string errorMessage)
+        {
+            return AddRule(item => propertySelector(item).CompareTo(value) <= 0, errorMessage);
+        }
+
+        public List<ValidationError> Validate(T value)
+        {
+            _validationErrors.Clear();
+
+            foreach (var predicate in _predicates)
+            {
+                if (!predicate(value))
+                {
+                    _validationErrors.Add(new ValidationError { PropertyName = "N/A", ErrorMessage = "N/A" });
+                }
+            }
+
+            foreach (var customValidationMethod in _customValidationMethods)
+            {
+                if (!customValidationMethod(value))
+                {
+                    _validationErrors.Add(new ValidationError { PropertyName = "N/A", ErrorMessage = "N/A" });
+                }
+            }
+
+            return _validationErrors;
+        }
+    }
+}
